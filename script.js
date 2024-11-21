5b376ce0eed182ee489525709efcff0bed0c18a6
kaducodes <kadu.codes@gmail.com>
2024-11-20 23:34:54 -0300

    commit da madruga 20/11/24
    organizei melhor a estrutura do site no html
    
    implementei outra lógica no javascript mas ainda não está fazendo as condicionais corretamente no js, corrigir depois!

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+var valorPadrao;
+var padrao = window.document.getElementById('txtpadrao').value
+if (padrao === "baixo") {
+    valorPadrao = 1800;
+} else if (padrao === "medio") {
+    valorPadrao = 2200;
+} else if (padrao === "alto") {
+    valorPadrao = 3000;
+} else {
+    valorPadrao = 0;
+}
+
 function indices() {
     var terreno = window.document.getElementById('txtterreno')
     var construida = window.document.getElementById('txtconstruida')
@@ -5,7 +17,7 @@ function indices() {
     var nterreno = Number(terreno.value)
     var nconstruida = Number(construida.value)
     var ia = (nconstruida / nterreno) * 100
-    var custo = nconstruida * 2500
+    var custo = nconstruida * valorPadrao
     var res = window.document.querySelector('div#res')
     res.innerHTML = `Resultados: <br>
                     Taxa de Uso de Solo = ${ia}% <br>
